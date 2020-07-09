64e4ebad1dd7848dee14b47d126fd5f3bef0e9be
Munish <50661000+munishshukla@users.noreply.github.com>
2020-07-10 00:08:47 +0530

    ui changes

--- background.js
+++ background.js
@@ -10,10 +10,16 @@ chrome.runtime.onInstalled.addListener(function() {
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
+      conditions: [new chrome.declarativeContent.PageStateMatcher()],
+      
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 });
+
+
+
+
+// {
+//   pageUrl: {hostEquals: 'developer.chrome.com'},
+// }
--- popup.js
+++ popup.js
@@ -3,19 +3,73 @@
 // found in the LICENSE file.
 
 'use strict';
+let menu = []
+let query = document.getElementById('query');
+menu.push(query);
+let country = document.getElementById('country');
+menu.push(country);
+let domain = document.getElementById('domain');
+menu.push(domain);
+let language = document.getElementById('language');
+menu.push(language);
+let file = document.getElementById('file');
+menu.push(file);
+let time = document.getElementById('time');
+menu.push(time);
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
 
-let changeColor = document.getElementById('changeColor');
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.executeScript(
+//         tabs[0].id,
+//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//   });
+// };
+for (let index = 0; index < menu.length; index++) {
+  const element = menu[index];
+  element.onclick = function(event) {
+    removeActiveClassFromMenu();
+    event.target.parentElement.classList.add("active");
+    document.getElementById(event.target.parentElement.id + "Heading").style.display = "block";
+    document.getElementById(event.target.parentElement.id + "Content").style.display = "block";
+  }
+}
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+function removeActiveClassFromMenu() {
+  for (let index = 0; index < menu.length; index++) {
+    const element = menu[index];
+    element.classList.remove("active");
+    document.getElementById(element.id + "Heading").style.display = "none";
+    document.getElementById(element.id + "Content").style.display = "none";    
+    
+  }
+}
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+// query.onclick = function(event) {
+//   console.log("query onclick");
+//   console.log(event.target.parentElement);
+// };
+
+// country.onclick = function(event) {
+
+// };
+
+// domain.onclick = function(event) {
+
+// };
+
+// language.onclick = function(event) {
+
+// };
+
+// file.onclick = function(event) {
+
+// };
+
+// time.onclick = function(event) {
+
+// };
